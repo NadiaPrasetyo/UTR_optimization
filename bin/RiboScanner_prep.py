f4abcfbb9ce13fc8382ccb49328a347a0f9e18e3
NadiaPrasetyo <nadiakinsellap@gmail.com>
2026-05-20 01:32:37 +0000

    adding sequence test for RiboScanner

---
+++ bin/RiboScanner_prep.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+import numpy as np
+import os
+import sys
+import argparse
+
+def main(input_file, output_file):
+    df = pd.read_csv(input_file)
+    df = df.replace(np.nan, '', regex=True)
+
+    # get the accession, accession,ensembl_gene_id,ensembl_transcript_id, sequence = utr5
+    df = df[['accession', 'ensembl_gene_id', 'ensembl_transcript_id', 'utr5']]
+    df.to_csv(output_file, sep='\t', index=False)
+
+
+    print("Done ! Now you can run:\n\tRiboScanner predict\n\t--input ./example_data/input.txt\n\t--column_sequence utr5\n\t--output ./output.txt")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required=True, type=str, help='Input CSV file')
+    parser.add_argument('-o', '--output', required=True, type=str, help='Output TSV file')
+    args = parser.parse_args()
+
+    if not os.path.exists(os.path.dirname(args.output)):
+        os.makedirs(os.path.dirname(args.output))
+
+    main(args.input, args.output)
