48ccb13af04a10538b91c0f880b20aceec0e9224
NadiaPrasetyo <nadiakinsellap@gmail.com>
2026-06-25 00:37:14 +0000

    adding cm finder script

---
+++ bin/cmfinder_RNAphylo_R2R.sh
@@ -0,0 +1,2 @@
+# CMfinder
+cmfinder-0.4.1.18/cmfinder04/cmfinder04 -o A7_analysis/phylogeny/${file}_branch/cmfinder_3UTR.sto -a A7_analysis/phylogeny/${file}_branch/3UTR.mafft.sto A7_analysis/phylogeny/${file}_branch_3utr.fasta
